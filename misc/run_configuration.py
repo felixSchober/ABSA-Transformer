27c25e73b605534b7dd7b1df61923210c6497abc
Felix Schober <github@felix-schober.com>
2019-02-22 20:41:57 +0100

    add random hyper parameter generator

--- misc/run_configuration.py
+++ misc/run_configuration.py
@@ -1,4 +1,5 @@
 from misc.utils import get_class_variable_table
+import random
 
 class RunConfiguration(object):
 
@@ -46,6 +47,7 @@ class RunConfiguration(object):
                 self.embedding_type = kwargs['embedding_type']
                 self.embedding_name = kwargs['embedding_name']
                 self.embedding_dim = kwargs['embedding_dim']
+                self.clip_comments_to = kwargs['clip_comments_to']
 
                 self.language = kwargs['language']
                 self.use_stop_words = kwargs['use_stop_words']
@@ -54,12 +56,68 @@ class RunConfiguration(object):
         def __str__(self):
                 return get_class_variable_table(self, 'Hyperparameters')
 
+def randomize_params(config, param_dict_range) -> RunConfiguration:
+
+        if 'batch_size' in param_dict_range:
+                ranges = param_dict_range['batch_size']
+                config.batch_size = random.randint(ranges[0], ranges[1])
+
+        if 'num_encoder_blocks' in param_dict_range:
+                ranges = param_dict_range['num_encoder_blocks']
+                config.n_enc_blocks = random.randint(ranges[0], ranges[1])
+
+        if 'pointwise_layer_size' in param_dict_range:
+                ranges = param_dict_range['pointwise_layer_size']
+                config.pointwise_layer_size = random.randint(ranges[0], ranges[1])
+
+        if 'clip_comments_to' in param_dict_range:
+                ranges = param_dict_range['clip_comments_to']
+                config.clip_comments_to = random.randint(ranges[0], ranges[1])
+
+        if 'learning_rate' in param_dict_range:
+                ranges = param_dict_range['learning_rate']
+                config.learning_rate = random.uniform(ranges[0], ranges[1])
+
+        if 'learning_rate_factor' in param_dict_range:
+                ranges = param_dict_range['learning_rate_factor']
+                config.learning_rate_factor = random.uniform(ranges[0], ranges[1])
+
+        if 'learning_rate_warmup' in param_dict_range:
+                ranges = param_dict_range['learning_rate_warmup']
+                config.learning_rate_warmup = random.uniform(ranges[0], ranges[1])
+
+        if 'optim_adam_beta1' in param_dict_range:
+                ranges = param_dict_range['optim_adam_beta1']
+                config.optim_adam_beta1 = random.uniform(ranges[0], ranges[1])
+
+        if 'optim_adam_beta2' in param_dict_range:
+                ranges = param_dict_range['optim_adam_beta2']
+                config.optim_adam_beta2 = random.uniform(ranges[0], ranges[1])
+
+        if 'dropout_rate' in param_dict_range:
+                ranges = param_dict_range['dropout_rate']
+                config.dropout_rate = random.uniform(ranges[0], ranges[1])
+
+        if 'transformer_config' in param_dict_range:
+                transformer_config = param_dict_range['transformer_config']
+
+                if 'transformer_heads' in transformer_config:                        
+                        config.n_heads = random.choice(transformer_config['transformer_heads'])
+
+                        # based on num_heads choose the d_v, d_k, d_q sizes
+                        # make sure that it will be a valid size
+                        assert(config.model_size % config.n_heads == 0, f"number of heads {config.n_heads} is not a valid number of heads for model size {config.model_size}.")
+
+                        config.d_k = config.model_size // config.n_heads
+                        config.d_v = config.model_size // config.n_heads
+
+        return config
 
 
-def get_default_params() -> RunConfiguration:
+def get_default_params(use_cuda: bool = False) -> RunConfiguration:
     return RunConfiguration(
-        batch_size=100,
-        model_size=256,
+        batch_size=12,
+        model_size=300,
         learning_rate_type='noam',
         learning_rate=0,
         learning_rate_factor=2,
@@ -67,19 +125,20 @@ def get_default_params() -> RunConfiguration:
         optim_adam_beta1=0.9,
         optim_adam_beta2=0.98,
         early_stopping=5,
-        num_epochs=10,
-        num_encoder_blocks=2,
-        num_heads=3,
-        att_d_k=100,
-        att_d_v=100,
+        num_epochs=1,
+        num_encoder_blocks=3,
+        num_heads=6,
+        att_d_k=50,
+        att_d_v=50,
         dropout_rate=0.1,
         pointwise_layer_size=2048,
-        log_every_xth_iteration=200,
-        embedding_type='glove',
+        log_every_xth_iteration=-1,
+        embedding_type='fasttext',
         embedding_dim=300,
         embedding_name='6B',
-        language='en',
+        language='de',
         use_stop_words=True,
-        use_cuda=False
+        use_cuda=use_cuda,
+        clip_comments_to=100
     )
 
