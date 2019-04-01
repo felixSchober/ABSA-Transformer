a82fff6679bf36f159aed331d90d84e8abfc22b2
Felix Schober <github@felix-schober.com>
2019-04-01 21:19:23 +0200

    update evaluator code

--- evaluator.py
+++ evaluator.py
@@ -1,22 +1,19 @@
 import logging
 import torch
 
-#from tqdm.autonotebook import tqdm
-
 from data.data_loader import Dataset
-from data.germeval2017 import germeval2017_dataset
-
+from data.germeval2017 import germeval2017_dataset as dsl
 from misc.preferences import PREFERENCES
 from misc.visualizer import *
-from misc.run_configuration import get_default_params, randomize_params, LearningSchedulerType
+from misc.run_configuration import get_default_params, randomize_params, OutputLayerType, hyperOpt_goodParams, elmo_params, good_organic_hp_params, default_params
 from misc import utils
 
-from optimizer import get_default_optimizer
+from optimizer import get_optimizer
 from criterion import NllLoss, LossCombiner
 
 from models.transformer.encoder import TransformerEncoder
 from models.jointAspectTagger import JointAspectTagger
-from models.transformer.train import Trainer
+from trainer.train import Trainer
 import pprint
 
 import torchtext
@@ -24,46 +21,48 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 PREFERENCES.defaults(
-    data_root='./data/germeval2017',
-    data_train='train_v1.4.tsv',    
-    data_validation='dev_v1.4.tsv',
-    data_test='test_TIMESTAMP1.tsv',
-    early_stopping='highest_5_F1'
+	data_root='./data/data/germeval2017',
+	data_train='train_v1.4.tsv',    
+	data_validation='dev_v1.4.tsv',
+	data_test='test_TIMESTAMP1.tsv',
+	source_index=0,
+	target_vocab_index=2,
+	file_format='csv'
 )
 def load(hp, logger):
-    dataset = Dataset(
-        'germeval',
-        logger,
-        hp,
-        source_index=0,
-        target_vocab_index=2,
-        data_path=PREFERENCES.data_root,
-        train_file=PREFERENCES.data_train,
-        valid_file=PREFERENCES.data_validation,
-        test_file=PREFERENCES.data_test,
-        file_format='.tsv',
-        init_token=None,
-        eos_token=None
-    )
-    dataset.load_data(germeval2017_dataset, verbose=False)
-    return dataset
+	dataset = Dataset(
+		hp.task,
+		logger,
+		hp,
+		source_index=PREFERENCES.source_index,
+		target_vocab_index=PREFERENCES.target_vocab_index,
+		data_path=PREFERENCES.data_root,
+		train_file=PREFERENCES.data_train,
+		valid_file=PREFERENCES.data_validation,
+		test_file=PREFERENCES.data_test,
+		file_format=PREFERENCES.file_format,
+		init_token=None,
+		eos_token=None
+	)
+	dataset.load_data(dsl, verbose=True)
+	return dataset
 
 def load_model(dataset, hp, experiment_name):
-    loss = LossCombiner(4, dataset.class_weights, NllLoss)
-    transformer = TransformerEncoder(dataset.source_embedding,
-                                     hyperparameters=hp)
-    model = JointAspectTagger(transformer, hp, 4, 20, dataset.target_names)
-    optimizer = get_default_optimizer(model, hp)
-    trainer = Trainer(
-                        model,
-                        loss,
-                        optimizer,
-                        hp,
-                        dataset,
-                        experiment_name,
-                        enable_tensorboard=False,
-                        verbose=False)
-    return trainer
+	loss = LossCombiner(4, dataset.class_weights, NllLoss)
+	transformer = TransformerEncoder(dataset.source_embedding,
+									 hyperparameters=hp)
+	model = JointAspectTagger(transformer, hp, 4, 20, dataset.target_names)
+	optimizer = get_optimizer(model, hp)
+	trainer = Trainer(
+						model,
+						loss,
+						optimizer,
+						hp,
+						dataset,
+						experiment_name,
+						enable_tensorboard=False,
+						verbose=True)
+	return trainer
 
 def produce_test_gold_labels(iterator: torchtext.data.Iterator, dataset: Dataset, filename='gold_labels.xml'):
 
@@ -165,13 +164,14 @@ def write_evaluation_file(iterator: torchtext.data.Iterator, dataset: Dataset, t
 
 
 
-PREFERENCES.defaults(
-    data_root='./data/germeval2017',
-    data_train='train_v1.4.tsv',    
-    data_validation='dev_v1.4.tsv',
-    data_test='test_TIMESTAMP1.tsv',
-    early_stopping='highest_5_F1'
-)
+
+
+
+
+
+
+
+
 # experiment_name = utils.create_loggers(experiment_name='testing')
 # logger = logging.getLogger(__name__)
 
@@ -183,28 +183,24 @@ PREFERENCES.defaults(
 # dataset = load(default_hp, logger)
 # produce_test_gold_labels(dataset.test_iter, dataset)
 
-experiment_name = 'HyperParameterTest'
+experiment_name = 'EvaluationTest'
 use_cuda = True
 experiment_name = utils.create_loggers(experiment_name=experiment_name)
 logger = logging.getLogger(__name__)
 
-hp = get_default_params(True)
+baseline = {**default_params, **hyperOpt_goodParams}
+rc = get_default_params(use_cuda=True, overwrite={}, from_default=baseline)
 
-hp.learning_rate_type = LearningSchedulerType.Noam
-hp.n_enc_blocks = 2
-hp.n_heads = 10
-hp.d_k = 30
-hp.d_v = 30
-hp.dropout_rate = 0.06955106610
-hp.pointwise_layer_size = 134
-hp.clip_comments_to = 391
-logger.info(hp)
-print(hp)
-dataset = load(hp, logger)
-trainer = load_model(dataset, hp, experiment_name)
-import os
-path = os.path.join(os.getcwd(), 'logs', 'GoodResults')
-print(path)
-trainer.load_model(custom_path=path)
+
+dataset = load_dataset(rc, dataset_logger, rc.task)
+
+trainer.load_model(custom_path='C:\\Users\\felix\\OneDrive\\Studium\\Studium\\6. Semester\\MA\\Project\\ABSA-Transformer\\logs\\GermEval7_Experiments\\20190401\\0\\checkpoints')
 trainer.set_cuda(True)
+result = trainer.perform_final_evaluation(use_test_set=True, verbose=False)
+
+# import os
+# path = os.path.join(os.getcwd(), 'logs', 'GoodResults')
+# print(path)
+# trainer.load_model(custom_path=path)
+# trainer.set_cuda(True)
 write_evaluation_file(dataset.test_iter, dataset, trainer)
