3bb31999470702bdc4ea366878dc359c8459cac3
Felix Schober <github@felix-schober.com>
2019-04-24 14:39:12 +0200

    rework main method

--- main.py
+++ main.py
@@ -1,176 +1,129 @@
-import matplotlib
-import copy
 import logging
-
-from data.data_loader import Dataset
-
 from misc.preferences import PREFERENCES
-from misc.run_configuration import get_default_params, randomize_params, OutputLayerType, hyperOpt_goodParams, elmo_params, good_organic_hp_params, default_params
+from misc.run_configuration import default_params, hyperOpt_goodParams
 from misc import utils
-
-from optimizer import get_optimizer
-from criterion import NllLoss, LossCombiner
-
-from models.transformer.encoder import TransformerEncoder
-from models.jointAspectTagger import JointAspectTagger
-from trainer.train import Trainer
-
 from misc.experimental_environment import Experiment
 from misc.transfer_learning_experiment import TransferLearningExperiment
+import argparse
+import traceback
 
-import pprint
+def run(args):
+	dataset_choice = args.dataset
+	runs = args.runs
+	epochs = args.epochs
+	name = args.name
+	description = args.description
+	task = args.task
 
-# PREFERENCES.defaults(
-#  	data_root='./data/data/germeval2017',
-#  	data_train='train_v1.4.tsv',    
-#  	data_validation='dev_v1.4.tsv',
-#  	data_test='test_TIMESTAMP1.tsv',
-#  	source_index=0,
-#  	target_vocab_index=2,
-#  	file_format='csv'
-# )
-# from data.germeval2017 import germeval2017_dataset as dsl
+	possible_dataset_values = ['germeval', 'organic', 'amazon']
+	if dataset_choice not in possible_dataset_values:
+		parser.error('The dataset argument was not in the allowed range of values: ' + str(possible_dataset_values))
 
+	if dataset_choice == possible_dataset_values[0]:
+		from data.germeval2017 import germeval2017_dataset as dsl
+		PREFERENCES.defaults(
+			data_root='./data/data/germeval2017',
+			data_train='train_v1.4.tsv',    
+			data_validation='dev_v1.4.tsv',
+			data_test='test_TIMESTAMP1.tsv',
+			source_index=0,
+			target_vocab_index=2,
+			file_format='csv',
+			language='de'
+		)
+		
 
-# from data.organic2019 import organic_dataset as dsl
-# from data.organic2019 import ORGANIC_TASK_ALL, ORGANIC_TASK_ENTITIES, ORGANIC_TASK_ATTRIBUTES, ORGANIC_TASK_ENTITIES_COMBINE, ORGANIC_TASK_COARSE
-# PREFERENCES.defaults(
-#    data_root='./data/data/organic2019',
-#    data_train='train.csv',    
-#    data_validation='validation.csv',
-#    data_test='test.csv',
-# 	source_index=0,
-# 	target_vocab_index=1,
-# 	file_format='csv'
-# )
+		specific_hp = {**hyperOpt_goodParams, **{
+			'task': 'germeval',
+			'use_stop_words': True,
+			'language': 'de',
+			'embedding_type': 'fasttext'
+		}}
 
-PREFERENCES.defaults(
-	data_root='./data/data/amazon/splits',
-	data_train='train.pkl',    
-	data_validation='val.pkl',
-	data_test='test.pkl',
-	source_index=0,
-	target_vocab_index=1,
-	file_format='pkl'
-)
-from data.amazon import amazon_dataset as dsl
+	elif dataset_choice == possible_dataset_values[1]:
+			from data.organic2019 import organic_dataset as dsl
+			from data.organic2019 import ORGANIC_TASK_ALL, ORGANIC_TASK_ENTITIES, ORGANIC_TASK_ATTRIBUTES, ORGANIC_TASK_ENTITIES_COMBINE, ORGANIC_TASK_COARSE
+			from misc.run_configuration import good_organic_hp_params
 
-# def load(hp, logger):
-# 	dataset = Dataset(
-# 		hp.task,
-# 		logger,
-# 		hp,
-# 		source_index=PREFERENCES.source_index,
-# 		target_vocab_index=PREFERENCES.target_vocab_index,
-# 		data_path=PREFERENCES.data_root,
-# 		train_file=PREFERENCES.data_train,
-# 		valid_file=PREFERENCES.data_validation,
-# 		test_file=PREFERENCES.data_test,
-# 		file_format=PREFERENCES.file_format,
-# 		init_token=None,
-# 		eos_token=None
-# 	)
-# 	dataset.load_data(dsl, verbose=True)
-# 	return dataset
+			possible_organic_values = [ORGANIC_TASK_ALL, ORGANIC_TASK_ENTITIES, ORGANIC_TASK_ATTRIBUTES, ORGANIC_TASK_ENTITIES_COMBINE, ORGANIC_TASK_COARSE]
+			if task not in possible_organic_values:
+				parser.error('The task argument was not in the allowed range of values: ' + str(possible_organic_values))
 
-# def load_model(dataset, hp, experiment_name):
-# 	loss = LossCombiner(4, dataset.class_weights, NllLoss)
-# 	transformer = TransformerEncoder(dataset.source_embedding,
-# 									 hyperparameters=hp)
-# 	model = JointAspectTagger(transformer, hp, 4, 20, dataset.target_names)
-# 	optimizer = get_optimizer(model, hp)
-# 	trainer = Trainer(
-# 						model,
-# 						loss,
-# 						optimizer,
-# 						hp,
-# 						dataset,
-# 						experiment_name,
-# 						enable_tensorboard=False,
-# 						verbose=True)
-# 	return trainer
+			PREFERENCES.defaults(
+				data_root='./data/data/organic2019',
+				data_train='train.csv',    
+				data_validation='validation.csv',
+				data_test='test.csv',
+				source_index=0,
+				target_vocab_index=1,
+				file_format='csv',
+				language='en'
+			)
+			specific_hp = good_organic_hp_params
+			specific_hp['task'] = task
+	
+	else:
+		PREFERENCES.defaults(
+			data_root='./data/data/amazon/splits',
+			data_train='train.pkl',    
+			data_validation='val.pkl',
+			data_test='test.pkl',
+			source_index=0,
+			target_vocab_index=1,
+			file_format='pkl',
+			language='en'
+		)
+		from data.amazon import amazon_dataset as dsl
+		from misc.run_configuration import hyperOpt_goodParams
 
-# experiment_name = 'testing'
-# use_cuda = True
-# experiment_name = utils.create_loggers(experiment_name=experiment_name)
-# logger = logging.getLogger(__name__)
-# logger.info('Current commit: ' + utils.get_current_git_commit())
+		specific_hp = {**hyperOpt_goodParams, **{
+			'task': 'amazon',
+			'use_spell_checkers': True,
+			'use_stop_words': True,
+			'language': 'en',
+			'clip_comments_to': 100,
+			'embedding_type': 'fasttext'
+		}}
 
-# hp = get_default_params(use_cuda=True, overwrite=hyperOpt_goodParams)
+	main_experiment_name = name
+	use_cuda = True
+	experiment_name = utils.create_loggers(experiment_name=main_experiment_name)
+	logger = logging.getLogger(__name__)
+	dataset_logger = logging.getLogger('data_loader')
+	logger.info('Run hyper parameter random grid search for experiment with name ' + main_experiment_name)
+	logger.info('num_optim_iterations: ' + str(runs))
+	specific_hp['epochs'] = epochs
 
+	try:
+		logger.info('Current commit: ' + utils.get_current_git_commit())
+		print('Current commit: ' + utils.get_current_git_commit())
+	except Exception as err:
+		logger.exception('Could not print current commit')
 
-# #hp.num_epochs = 35
-# hp.log_every_xth_iteration = 10
-# #hp.language = 'de'
-# # hp.adam_weight_decay = 1e-4
-# # hp.pointwise_layer_size = 256
-# # hp.n_enc_blocks = 2
-# #hp.embedding_type = 'glove'
-# #hp.language = 'en'
-# hp.use_spell_checkers = True
-# #hp.clip_comments_to = 300
-# #hp.embedding_type = 'fasttext'  
-# logger.info(hp)
-# print(hp)
+	try:
+		e = Experiment(name, description, default_params, specific_hp, dsl, runs=runs)
+		e.run()
+	except Exception as err:
+		logger.exception('Could not complete run')
+		print('Could not complete run. The log file provides more details.')
+		print(repr(err))
+		traceback.print_tb(err.__traceback__)
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='HyperOpt hp optimization tool')
+	parser.add_argument('dataset', type=str,
+						help='Specify which dataset to optimize')
+	parser.add_argument('--runs', type=int, default=1,
+						help='Number of runs evaluation runs to perform')
+	parser.add_argument('--epochs', type=int, default=35,
+						help='Number of epochs to perform')
+	parser.add_argument('--name', type=str, default='test',
+						help='Specify a name of the optimization run')
+	parser.add_argument('--description', type=str, default='test run on {} with {} epochs and {} validations',
+						help='Specify a name of the optimization run')
+	parser.add_argument('--task', type=str,
+						help='Specify the task to execute. Only applicable when using the organic dataset')
+	args = parser.parse_args()
 
-# dataset_logger = logging.getLogger('data_loader')
-
-# logger.debug('Load dataset')
-# dataset = load(hp, dataset_logger)
-# logger.debug('dataset loaded')
-# logger.debug('Load model')
-# trainer = load_model(dataset, hp, experiment_name)
-# logger.debug('model loaded')
-
-# logger.debug('Begin training')
-# model = None
-# try:
-# 	#trainer.load_model(custom_path='C:\\Users\\felix\\OneDrive\\Studium\\Studium\\6. Semester\\MA\\Project\\ABSA-Transformer\\logs\\baseline\\20190323\\0\\checkpoints')
-# 	#trainer.set_cuda(True)
-# 	result = trainer.train(use_cuda=hp.use_cuda, perform_evaluation=False)
-# 	model = result['model']
-# except Exception as err:
-# 	logger.exception("Could not complete iteration because of " + str(err))
-# 	print(f'Could not complete iteration because of {str(err)}')
-
-# # perform evaluation and log results
-# result = None
-# try:
-# 	result = trainer.perform_final_evaluation(use_test_set=True, verbose=True)
-# except Exception as err:
-# 	logger.exception("Could not complete iteration evaluation for it " + str(err))
-# 	print(f'Could not complete iterationevaluation because of {str(err)}')
-
-
-
-
-#trainer.load_model()
-
-#trainer.set_cuda(True)
-#result_labels = trainer.classify_sentence('Die Bahn preise sind sehr billig')
-
-#result = trainer.train(use_cuda=hp.use_cuda, perform_evaluation=False)
-#trainer.perform_final_evaluation(False)
-
-#evaluation_results = trainer.perform_final_evaluation()
-
-test_params = {**hyperOpt_goodParams, **{'num_epochs': 1, 'language': 'en', 'batch_size': 12, 'task': 'ORGANIC_TASK_COARSE', 'token_removal_2': True}}
-e = Experiment('t3st', 'test description', default_params, test_params, dsl, runs=1)
-e.run()
-# from data.organic2019 import load_splits as dsl
-# loaders = [dsl, dsl]
-# dataset_infos = {
-# 	'source_index': [0, 0],
-# 	'target_vocab_index': [1, 1],
-# 	'data_root': ['./data/data/organic2019', './data/data/organic2019'],
-# 	'data_train': ['train.csv', 'train.csv'],
-# 	'data_validation': ['validation.csv', 'validation.csv'],
-# 	'data_test': ['test.csv', 'test.csv'],
-# 	'file_format': ['csv', 'csv']
-# }
-# test_params = {**hyperOpt_goodParams, **{'num_epochs': 1, 'language': 'en'}}
-# e = TransferLearningExperiment(ORGANIC_TASK_ATTRIBUTES, 't3st', 'test description', default_params, test_params, loaders, dataset_infos, runs=1)
-# e.run()
-
-print('Exit')
+	run(args)
+	print('Exit')
